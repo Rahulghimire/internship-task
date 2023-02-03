c7db516a79785307c44d526d5327cacd2925e0aa
raul <ghimirerahul554@gmail.com>
2023-02-03 11:24:32 +0545

    project path updated

--- JS-Assessment/Assessment-4i/index.js
+++ JS-Assessment/Assessment-4i/index.js
@@ -2,18 +2,19 @@ const CONTAINER_WIDTH = 500;
 const CONTAINER_HEIGHT = 500;
 const BOX_COUNT = 5;
 const SPEED = 10;
-let BOX_WIDTH = 50;
-let BOX_HEIGHT = 50;
+const BOX_WIDTH = 50;
+const BOX_HEIGHT = 50;
 
 function main() {
   let body = document.querySelector("body");
   const boxLists = initializeProgram(body);
-  startProgram(boxLists);
+  //startProgram(boxLists);
 }
+
 function initializeProgram(body) {
   const boxContainer = createBoxContainer(body);
   const boxLists = createBoxes(boxContainer);
-  return boxLists;
+  //return boxLists;
 }
 
 function createBoxContainer(body) {
@@ -25,13 +26,9 @@ function createBoxContainer(body) {
   body.appendChild(boxContainer);
   return boxContainer;
 }
-// function getPosition(container, box) {
-//   return Math.floor(Math.random() * container - box);
-// }
-// return Math.floor((Math.random() * coordinate) / SPEED) * SPEED;
 
-function getPosition(coordinate) {
-  return Math.floor((Math.random() * coordinate) / SPEED) * SPEED;
+function getPosition(value) {
+  return Math.floor(((Math.random() * value) / SPEED) * SPEED);
 }
 
 function createBoxes(boxContainer) {
@@ -39,75 +36,25 @@ function createBoxes(boxContainer) {
   for (let i = 0; i < BOX_COUNT; i++) {
     let getPositionX = getPosition(CONTAINER_WIDTH);
     let getPositionY = getPosition(CONTAINER_HEIGHT);
-    console.log(getPositionX, getPositionY);
+    console.log(getPositionX, getPositionY, i);
+    let boxObj = createBox(getPositionX, getPositionY, i, boxContainer);
     let box = document.createElement("div");
     box.setAttribute("id", `box-${i + 1}`);
     box.classList.add("box");
     box.style.height = BOX_HEIGHT + "px";
     box.style.width = BOX_WIDTH + "px";
     boxContainer.appendChild(box);
+    boxLists.push(boxObj);
   }
-  boxContainer.append(box);
-  boxLists.push(box);
 }
-function createBox() {}
+// function createBox(posX, posY, counter, boxContainer) {
+//   const boxObj = {};
 
-// function startProgram(list) {
-//   let boxList = list;
-//   setInterval(animateBox, 50);
-//   function animateBox() {
-//     for (let i = 0; i < boxList.length; i++) {
-//       box = boxList[i];
-//       let coordinateX = parseInt(box.style.left);
-//       let coordinateY = parseInt(box.style.top);
-//       let directionX = 1;
-//       let directionY = 1;
-//       // check x coordinate
-//       if (coordinateX >= CONTAINER_WIDTH - BOX_WIDTH) {
-//         directionX = -1;
-//         coordinateX = coordinateX + SPEED * directionX;
-//         box.style.left = `${coordinateX}px`;
-//         // boxList[i] = box;
-//       } else if (coordinateX <= 0) {
-//         directionX = 1;
-//         coordinateX = coordinateX + SPEED * directionX;
-//         box.style.left = `${coordinateX}px`;
-//         // boxList[i] = box;
-//       }
-
-//       // check y coordinate
-//       if (coordinateY >= CONTAINER_HEIGHT - BOX_HEIGHT) {
-//         directionY = -1;
-//         coordinateY = coordinateY + SPEED * directionY;
-//         box.style.top = `${coordinateY}px`;
-//         // boxList[i] = box;
-//       } else if (coordinateY <= 0) {
-//         directionY = 1;
-//         coordinateY = coordinateY + SPEED * directionY;
-//         box.style.top = `${coordinateY}px`;
-//         // boxList[i] = box;
-//       }
-//       coordinateX = coordinateX + SPEED * directionX;
-//       coordinateY = coordinateY + SPEED * directionY;
-//       box.style.left = `${coordinateX}px`;
-//       box.style.top = `${coordinateY}px`;
-//       // boxList[i] = box;
-//     }
-//   }
-//   return boxList;
+//   return boxObj;
 // }
 
 main();
 
-// function cerateboses(boxes){
-//     for i=0 to 10
-//     const posx=i*10;
-//     const posty=(i+1)*10;
-//     const box={
-//         positionx:positionx;
-//         positionx:posy;
-//     }
-// }
 // function drawboxes()
 
 // main function
